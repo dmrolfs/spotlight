6f1ba86a7bc98b2e9d0b9d3dff3d27c2c7061905
Damon Rolfs <drolfs@gmail.com>
2015-12-03 23:42:20 -0800

    added error handler to flow

--- core/src/main/scala/lineup/stream/GraphiteSerializationProtocol.scala
+++ core/src/main/scala/lineup/stream/GraphiteSerializationProtocol.scala
@@ -1,7 +1,10 @@
 package lineup.stream
 
+import java.nio.charset.Charset
+import akka.stream.{ActorAttributes, Supervision}
 import akka.stream.scaladsl.Flow
 import akka.util.ByteString
+import com.typesafe.scalalogging.LazyLogging
 import lineup.model.timeseries.TimeSeries
 
 
@@ -9,8 +12,32 @@ import lineup.model.timeseries.TimeSeries
   * Created by rolfsd on 11/25/15.
   */
 trait GraphiteSerializationProtocol {
-  def charset: String = ByteString.UTF_8
+//  def charset: Charset = Charset forName ByteString.UTF_8
   def framingFlow( maximumFrameLength: Int ): Flow[ByteString, ByteString, Unit]
-  def loadTimeSeriesData: Flow[ByteString, TimeSeries, Unit] = Flow[ByteString] mapConcat { toDataPoints }
   def toDataPoints( bytes: ByteString ): List[TimeSeries]
+  def loadTimeSeriesData: Flow[ByteString, TimeSeries, Unit] = {
+    Flow[ByteString]
+    .mapConcat { toDataPoints }
+    .withAttributes( ActorAttributes.supervisionStrategy(GraphiteSerializationProtocol.decider) )
+  }
+}
+
+object GraphiteSerializationProtocol extends LazyLogging{
+  trait ProtocolException extends Exception {
+    def part: String
+    def value: Any
+  }
+
+  val decider: Supervision.Decider = {
+    case ex: ProtocolException => {
+      logger.warn(
+        "ignoring message - could not parse " +
+        s"part:[${ex.part}] message-value:[${ex.value.toString}] type:[${ex.value.getClass.getName}]"
+      )
+
+      Supervision.Resume
+    }
+
+    case _ => Supervision.Stop
+  }
 }
