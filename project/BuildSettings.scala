70db2939daf8a996c7f8a6d5fc0edd327988f7ec
Damon Rolfs <drolfs@gmail.com>
2016-07-06 11:06:44 -0700

    bump versions

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -5,7 +5,7 @@ import sbt._
 import spray.revolver.RevolverPlugin._
 
 object BuildSettings {
-  val VERSION = "1.2.5"
+  val VERSION = "1.2.6"
 
   val defaultBuildSettings = Defaults.coreDefaultSettings ++ Format.settings ++ Revolver.settings ++ Seq(
     version := VERSION,
