199cc0549be42e85e6744d7e3062ee41c2f101ef
Damon Rolfs <drolfs@gmail.com>
2016-07-12 20:38:07 -0700

    bump versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Dependencies {
   object peds {
-    val version = "0.3.1"
+    val version = "0.3.2-SNAPSHOT"
     def module( id: String ) = "com.github.dmrolfs" %% s"peds-$id" % version
     def all = Seq( commons, akka, archetype )
 
