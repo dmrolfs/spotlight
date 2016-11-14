b105c976744e8c8093bc7fea8acd4627fc0b90c7
Damon Rolfs <drolfs@gmail.com>
2016-11-14 15:36:14 -0800

    set versions for release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Dependencies {
   object peds {
-    val version = "0.4.3-SNAPSHOT"
+    val version = "0.4.2"
     def module( id: String ) = "com.github.dmrolfs" %% s"peds-$id" % version
     def all = Seq( commons, akka, archetype )
 
@@ -14,7 +14,7 @@ object Dependencies {
   }
 
   object demesne {
-    val version = "2.0.3-SNAPSHOT"
+    val version = "2.0.2"
     def module( id: String ) = "com.github.dmrolfs" %% s"demesne-$id" % version
     val core = module( "core" )
     val testkit = module( "testkit" )
