3112dfa3bacee0733ca3d93f01214464b8f35d0d
Damon Rolfs <drolfs@gmail.com>
2016-11-14 15:43:40 -0800

    bumped version reference

--- tools/spotlight-local.sh
+++ tools/spotlight-local.sh
@@ -2,7 +2,7 @@
 
 mkdir ./target/persistence/shared-journal ./target/persistence/snapshots
 
-java -cp graphite/target/scala-2.11/com.github.dmrolfs-spotlight-graphite-2.0.3-SNAPSHOT.jar \
+java -cp graphite/target/scala-2.11/com.github.dmrolfs-spotlight-graphite-2.0.3.jar \
   -Dconfig.resource=application.conf \
   -Djava.library.path=native \
   -javaagent:coreos/aspectjweaver-1.8.8.jar \
