8daa61a610ca73fd4abf72aa9d4572413c11b584
Damon Rolfs <drolfs@gmail.com>
2017-03-20 16:52:42 -0700

    broke out ClusterRole

---
+++ core/src/main/scala/spotlight/infrastructure/ClusterRole.scala
@@ -0,0 +1,8 @@
+package spotlight.infrastructure
+
+/**
+  * Created by rolfsd on 3/20/17.
+  */
+class ClusterRole {
+
+}
--- core/src/main/scala/spotlight/infrastructure/package.scala
+++ core/src/main/scala/spotlight/infrastructure/package.scala
@@ -3,17 +3,7 @@ package spotlight
 import scala.collection.immutable.IndexedSeq
 import akka.Done
 import demesne.StartTask
-import enumeratum._
 
 package object infrastructure {
-  sealed abstract class ClusterRole( override val entryName: String ) extends EnumEntry
-
-  object ClusterRole extends Enum[ClusterRole] {
-    override val values: IndexedSeq[ClusterRole] = findValues
-
-    case object Control extends ClusterRole( "control" )
-    case object Worker extends ClusterRole( "worker" )
-  }
-
   // val kamonStartTask: StartTask = StartTask.withFunction( "start Kamon monitoring" ) { bc ⇒ kamon.Kamon.start(); Done }
 }
