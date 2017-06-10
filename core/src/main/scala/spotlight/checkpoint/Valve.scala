0a0fdeac07435cb1e318309707c7bb19f0fcd880
Damon Rolfs <drolfs@gmail.com>
2017-06-09 22:04:42 -0700

    more valve — didn’t see this remainder before last commit

--- core/src/main/scala/spotlight/checkpoint/Valve.scala
+++ core/src/main/scala/spotlight/checkpoint/Valve.scala
@@ -1,178 +1,210 @@
 package spotlight.checkpoint
 
-import scala.concurrent.Future
-import scala.concurrent.duration.{FiniteDuration, MILLISECONDS}
-import scala.util.{Failure, Success}
-import akka.Done
-import akka.actor.ActorRef
-import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
-import cats.syntax.show._
-import cats.data.{Kleisli, NonEmptyList}
+import scala.collection.immutable
+import akka.stream.{ Attributes, FlowShape, Inlet, Outlet }
+import akka.stream.stage._
 import com.persist.logging._
-import org.joda.{time => joda}
-import omnibus.akka.envelope._
-import demesne.{AggregateRootType, DomainModel}
-import spotlight.showImplicits._
-import spotlight.SpotlightProtocol
-import spotlight.analysis.algorithm.Algorithm
-import spotlight.checkpoint.CheckpointProtocol.Internal._
+import enumeratum._
 
 
-/**
-  * Created by rolfsd on 5/17/17.
+trait ValveSwitch {
+  def open: Unit
+  def close: Unit
+}
+
+/** Created by rolfsd on 6/8/17 from gist by regis leray
   */
-class CheckpointStage( model: DomainModel )
-extends GraphStage[FlowShape[SpotlightProtocol, SpotlightProtocol]] with ClassLogging {
+object Valve {
+  sealed trait Mode extends EnumEntry
 
-  val in = Inlet[SpotlightProtocol]( "CheckpointStage.in" )
-  val out = Outlet[SpotlightProtocol]( "CheckpointStage.out" )
-  val shape = FlowShape.of( in, out )
+  object Mode extends Enum[Mode] {
+    override def values: immutable.IndexedSeq[Mode] = findValues
 
-  override def createLogic( inheritedAttributes: Attributes ): GraphStageLogic = {
-    new GraphStageLogic( shape ) {
-      setHandler(
-        in,
-        new InHandler {
-          override def onPush(): Unit = {
-            grab( in ) match {
-              case c @ Checkpoint( token, initiator ) => {
-                val start = joda.DateTime.now
+    case object Open extends Mode
+    case object Closed extends Mode
+  }
+}
 
-                dispatchCheckpoint( c )
-                .onComplete {
-                  case Success( c ) => {
-                    val leadTime = leadTimeFrom( start )
-                    log.debug(
-                      Map(
-                        "@msg" -> "checkpoint success",
-                        "checkpoint" -> Map(
-                          "timestamp" -> start.show,
-                          "lead-time" -> leadTime.show,
-                          "token" -> token,
-                          "initiator" -> initiator.show
-                        )
-                      )
-                    )
+class Valve[A]( startMode: Valve.Mode = Valve.Mode.Open )
+    extends GraphStageWithMaterializedValue[FlowShape[A, A], ValveSwitch] with ClassLogging {
 
-                    initiator ! CheckpointSuccess( start, leadTime, token )
-                    pull( in )
-                  }
+  val in = Inlet[A]( "valve.in" )
+  val out = Outlet[A]( "valve.out" )
+  override val shape: FlowShape[A, A] = FlowShape( in, out )
 
-                  case Failure( ex ) => {
-                    val leadTime = leadTimeFrom( start )
+  override def createLogicAndMaterializedValue( inheritedAttributes: Attributes ): ( GraphStageLogic, ValveSwitch ) = {
+    val logic = new ValveGraphStageLogic( startMode )
+    ( logic, logic.switch )
+  }
 
-                    log.error(
-                      Map(
-                        "@msg" -> "checkpoint failure",
-                        "checkpoint" -> Map(
-                          "timestamp" -> start.show,
-                          "error-lead-time" -> leadTime.show,
-                          "token" -> token,
-                          "initiator" -> initiator.show
-                        )
-                      ),
-                      ex
-                    )
+  private class ValveGraphStageLogic( var mode: Valve.Mode ) extends GraphStageLogic( shape ) with InHandler with OutHandler {
+    setHandlers( in, out, this )
 
-                    initiator ! CheckpointFailure( start, leadTime, token, ex )
-                    pull( in )
-                  }
-                }
-              }
+    private var bufferedElement: Option[A] = None
 
-              case e => push( out, e )
-            }
+    override def onPush(): Unit = {
+      val element = grab( in )
+      mode match {
+        case Valve.Mode.Open ⇒ push( out, element )
+        case Valve.Mode.Closed ⇒ bufferedElement = Some( element )
+      }
+    }
 
-          }
-        }
-      )
+    override def onPull(): Unit = pull( in )
 
-      setHandler( out, new OutHandler { override def onPull(): Unit = pull( in ) } )
+    val switch: ValveSwitch = new ValveSwitch {
+      val callback: AsyncCallback[A] = getAsyncCallback[A] { push( out, _ ) }
 
-      def leadTimeFrom( start: joda.DateTime ): FiniteDuration = {
-        FiniteDuration( System.currentTimeMillis() - start.getMillis, MILLISECONDS )
+      override def open: Unit = {
+        mode = Valve.Mode.Open
+        bufferedElement foreach callback.invoke
+        bufferedElement = None
       }
 
-      type COp[A, R] = Kleisli[Future, A, R]
-
-      val findSavePoints: COp[Checkpoint, Set[Algorithm.TID]] = Kleisli[Future, Checkpoint, Set[Algorithm.TID]] { c => ??? }
-
-//      var dispatched: Set[Algorithm.TID] = Set.empty[Algorithm.TID]
-//      var responses: Set[SavePointResponse] = Set.empty[SavePointResponse]
-
-      val dispatch: COp[(Checkpoint, Map[AggregateRootType, Set[Algorithm.TID]]), Map[ActorRef, SavePointResponse]] = {
-        Kleisli[Future, (Checkpoint, Map[AggregateRootType, Set[Algorithm.TID]]), Map[ActorRef, SavePointResponse]] {
-          case (c, algorithmRootIds) => {
-            val savepoints: Set[(Algorithm.TID, Future[])] = for {
-              rootIds <- algorithmRootIds.toSet
-              (rt, tids) = rootIds
-              tid <- tids
-            } yield {
-              val ref = model( rt, tid.id )
-              val cmd = rt.snapshot map { _ saveSnapshotCommand tid } getOrElse { SaveSna}
-              ref.sendEnvelope( demesne.SaveSnapshot( tid ) )( c.sourceRef )
-              ( tid, savepoint )
-            }
-          }
-        }
-      }
-
-      val reduce: COp[(Checkpoint, Map[ActorRef, SavePointResponse]), (Checkpoint, CheckpointResponse)] = {
-        Kleisli[Future, (Checkpoint, Map[ActorRef, SavePointResponse]), (Checkpoint, CheckpointResponse)] {
-          case (c, saveResponses) => Future successful {
-            val failures = saveResponses.values collect { case r: SavePointFailure => SavePointError( r.savePoint, r.error ) }
-
-            val result = {
-              val leadTime = leadTimeFrom( c.start )
-              if ( failures.isEmpty ) CheckpointSuccess( c.start, leadTime, c.token )
-              else {
-                CheckpointFailure( c.start, leadTime, c.token, NonEmptyList(failures.head, failures.tail.toList) )
-              }
-            }
-
-            ( c, result )
-          }
-        }
-      }
-
-      type CReqResp = (Checkpoint, CheckpointResponse)
-      val logReponse: COp[CReqResp, CReqResp] = Kleisli[Future, CReqResp, CReqResp] { case (req, resp) =>
-        Future successful {
-          val cpBase = Map( "start" -> resp.start.show, "token" -> resp.token, "source" -> req.sourceRef )
-
-          resp match {
-            case success: CheckpointSuccess => {
-              log.debug(
-                Map(
-                  "@msg" -> "checkpoint success",
-                  "checkpoint" -> ( cpBase + ("lead-time" -> success.leadTime.show) )
-                )
-              )
-            }
-
-            case failure: CheckpointFailure => {
-              log.error(
-                Map(
-                  "@msg" -> "checkpoint failure",
-                  "checkpoint" -> ( cpBase + ("error-lead-time" -> resp.leadTime.show) ),
-                  "failures" -> failure.show
-                )
-              )
-            }
-          }
-
-          (req, resp)
-        }
-      }
-
-      val notifyAndPull: COp[CReqResp, Done] = Kleisli[Future, CReqResp, Done] { case (req, resp) =>
-        Future successful {
-          req.sourceRef ! resp
-          pull( in )
-          Done
-        }
-      }
+      override def close: Unit = { mode = Valve.Mode.Closed }
     }
   }
 }
+
+//      setHandler(
+//        in,
+//        new InHandler {
+//          override def onPush(): Unit = {
+//            grab( in ) match {
+//              case c @ Checkpoint( token, initiator ) => {
+//                val start = joda.DateTime.now
+//
+//                dispatchCheckpoint( c )
+//                .onComplete {
+//                  case Success( c ) => {
+//                    val leadTime = leadTimeFrom( start )
+//                    log.debug(
+//                      Map(
+//                        "@msg" -> "checkpoint success",
+//                        "checkpoint" -> Map(
+//                          "timestamp" -> start.show,
+//                          "lead-time" -> leadTime.show,
+//                          "token" -> token,
+//                          "initiator" -> initiator.show
+//                        )
+//                      )
+//                    )
+//
+//                    initiator ! CheckpointSuccess( start, leadTime, token )
+//                    pull( in )
+//                  }
+//
+//                  case Failure( ex ) => {
+//                    val leadTime = leadTimeFrom( start )
+//
+//                    log.error(
+//                      Map(
+//                        "@msg" -> "checkpoint failure",
+//                        "checkpoint" -> Map(
+//                          "timestamp" -> start.show,
+//                          "error-lead-time" -> leadTime.show,
+//                          "token" -> token,
+//                          "initiator" -> initiator.show
+//                        )
+//                      ),
+//                      ex
+//                    )
+//
+//                    initiator ! CheckpointFailure( start, leadTime, token, ex )
+//                    pull( in )
+//                  }
+//                }
+//              }
+//
+//              case e => push( out, e )
+//            }
+//
+//          }
+//        }
+//      )
+//
+//      setHandler( out, new OutHandler { override def onPull(): Unit = pull( in ) } )
+//
+//      def leadTimeFrom( start: joda.DateTime ): FiniteDuration = {
+//        FiniteDuration( System.currentTimeMillis() - start.getMillis, MILLISECONDS )
+//      }
+//
+//      type COp[A, R] = Kleisli[Future, A, R]
+//
+//      val findSavePoints: COp[Checkpoint, Set[Algorithm.TID]] = Kleisli[Future, Checkpoint, Set[Algorithm.TID]] { c => ??? }
+//
+////      var dispatched: Set[Algorithm.TID] = Set.empty[Algorithm.TID]
+////      var responses: Set[SavePointResponse] = Set.empty[SavePointResponse]
+//
+//      val dispatch: COp[(Checkpoint, Map[AggregateRootType, Set[Algorithm.TID]]), Map[ActorRef, SavePointResponse]] = {
+//        Kleisli[Future, (Checkpoint, Map[AggregateRootType, Set[Algorithm.TID]]), Map[ActorRef, SavePointResponse]] {
+//          case (c, algorithmRootIds) => {
+//            val savepoints: Set[(Algorithm.TID, Future[])] = for {
+//              rootIds <- algorithmRootIds.toSet
+//              (rt, tids) = rootIds
+//              tid <- tids
+//            } yield {
+//              val ref = model( rt, tid.id )
+//              val cmd = rt.snapshot map { _ saveSnapshotCommand tid } getOrElse { SaveSna}
+//              ref.sendEnvelope( demesne.SaveSnapshot( tid ) )( c.sourceRef )
+//              ( tid, savepoint )
+//            }
+//          }
+//        }
+//      }
+//
+//      val reduce: COp[(Checkpoint, Map[ActorRef, SavePointResponse]), (Checkpoint, CheckpointResponse)] = {
+//        Kleisli[Future, (Checkpoint, Map[ActorRef, SavePointResponse]), (Checkpoint, CheckpointResponse)] {
+//          case (c, saveResponses) => Future successful {
+//            val failures = saveResponses.values collect { case r: SavePointFailure => SavePointError( r.savePoint, r.error ) }
+//
+//            val result = {
+//              val leadTime = leadTimeFrom( c.start )
+//              if ( failures.isEmpty ) CheckpointSuccess( c.start, leadTime, c.token )
+//              else {
+//                CheckpointFailure( c.start, leadTime, c.token, NonEmptyList(failures.head, failures.tail.toList) )
+//              }
+//            }
+//
+//            ( c, result )
+//          }
+//        }
+//      }
+//
+//      type CReqResp = (Checkpoint, CheckpointResponse)
+//      val logReponse: COp[CReqResp, CReqResp] = Kleisli[Future, CReqResp, CReqResp] { case (req, resp) =>
+//        Future successful {
+//          val cpBase = Map( "start" -> resp.start.show, "token" -> resp.token, "source" -> req.sourceRef )
+//
+//          resp match {
+//            case success: CheckpointSuccess => {
+//              log.debug(
+//                Map(
+//                  "@msg" -> "checkpoint success",
+//                  "checkpoint" -> ( cpBase + ("lead-time" -> success.leadTime.show) )
+//                )
+//              )
+//            }
+//
+//            case failure: CheckpointFailure => {
+//              log.error(
+//                Map(
+//                  "@msg" -> "checkpoint failure",
+//                  "checkpoint" -> ( cpBase + ("error-lead-time" -> resp.leadTime.show) ),
+//                  "failures" -> failure.show
+//                )
+//              )
+//            }
+//          }
+//
+//          (req, resp)
+//        }
+//      }
+//
+//      val notifyAndPull: COp[CReqResp, Done] = Kleisli[Future, CReqResp, Done] { case (req, resp) =>
+//        Future successful {
+//          req.sourceRef ! resp
+//          pull( in )
+//          Done
+//        }
+//      }
--- core/src/test/scala/spotlight/checkpoint/ValveSpec.scala
+++ core/src/test/scala/spotlight/checkpoint/ValveSpec.scala
@@ -1,8 +1,86 @@
 package spotlight.checkpoint
 
-/**
-  * Created by rolfsd on 6/8/17.
-  */
-class ValveSpec {
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration._
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{ Keep, Sink, Source }
+import akka.stream.{ ActorMaterializer, Materializer }
+import akka.stream.testkit.scaladsl.TestSink
+import demesne.testkit.concurrent.CountDownFunction
+import org.scalatest._
+import org.scalatest.Matchers._
+import org.scalatest.concurrent.ScalaFutures
 
+/** Created by rolfsd on 6/8/17 from gist by regis leray
+  */
+class ValveSpec extends FlatSpec with ScalaFutures {
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: Materializer = ActorMaterializer()
+  implicit val ec: ExecutionContext = materializer.executionContext
+
+  "A closed valve" should "emit only " in {
+    val ( valve, seq ) = {
+      Source( 1 to 3 )
+        .viaMat( new Valve( Valve.Mode.Closed ) )( Keep.right )
+        .toMat( Sink.seq )( Keep.both )
+        .run()
+    }
+
+    val countDown = new CountDownFunction[String]
+    countDown await 200.millis
+    valve.open
+
+    whenReady( seq, timeout( 200.millis ) ) { sum ⇒ sum should contain inOrder ( 1, 2, 3 ) }
+  }
+
+  "A closed valve" should "emit only 5 elements after it has been open" in {
+    val ( valve, probe ) = {
+      Source( 1 to 5 )
+        .viaMat( new Valve( Valve.Mode.Closed ) )( Keep.right )
+        .toMat( TestSink.probe[Int] )( Keep.both )
+        .run()
+    }
+
+    probe request 2
+    probe expectNoMsg 100.millis
+
+    valve.open
+
+    probe.expectNext shouldEqual 1
+    probe.expectNext shouldEqual 2
+
+    probe request 3
+    probe.expectNext shouldEqual 3
+    probe.expectNext shouldEqual 4
+    probe.expectNext shouldEqual 5
+
+    probe.expectComplete()
+  }
+
+  it should "emit 5 elements after it has been open/close/open" in {
+    val ( valve, probe ) = {
+      Source( 1 to 5 )
+        .viaMat( new Valve )( Keep.right )
+        .toMat( TestSink.probe[Int] )( Keep.both )
+        .run()
+    }
+
+    probe request 2
+    probe.expectNext shouldEqual 1
+    probe.expectNext shouldEqual 2
+
+    valve.close
+
+    probe request 1
+    probe expectNoMsg 100.millis
+
+    valve.open
+    probe.expectNext shouldEqual 3
+
+    probe request 2
+    probe.expectNext shouldEqual 4
+    probe.expectNext shouldEqual 5
+
+    probe.expectComplete()
+  }
 }
